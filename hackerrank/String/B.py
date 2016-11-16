8a9499c51a59a8cccb2155ea63ec554470b4faa3
Simon Park <psean21c@gmail.com>
2016-11-15 20:10:42 -0500

    Create B.py

---
+++ hackerrank/String/B.py
@@ -0,0 +1,21 @@
+```
+@author: Simon Park
+version 1
+```
+string = input()
+
+lst_cmd = [i for i in input().split(" ")]
+idx = int(lst_cmd[:1][0])
+val = lst_cmd[1:][0]
+
+string = string[:idx] + val + string[idx+1:]
+
+print(string)
+
+```
+version 2
+```
+S=input()
+i,c=map(str,input().split())
+i=int(i)
+print(S[:i] + c + S[i+1:])
