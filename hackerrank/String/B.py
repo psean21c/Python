f6c1ba2fc07e5cc83f1988587b20a69ea1845025
Simon Park <psean21c@gmail.com>
2016-11-15 20:11:19 -0500

    Update B.py

--- hackerrank/String/B.py
+++ hackerrank/String/B.py
@@ -1,7 +1,7 @@
-```
+'''
 @author: Simon Park
 version 1
-```
+'''
 string = input()
 
 lst_cmd = [i for i in input().split(" ")]
@@ -12,9 +12,9 @@ string = string[:idx] + val + string[idx+1:]
 
 print(string)
 
-```
+'''
 version 2
-```
+'''
 S=input()
 i,c=map(str,input().split())
 i=int(i)
